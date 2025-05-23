e94348a9bfd293490a73635e387463ba070cdfe4
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-05-23 16:09:35 -0300

    configurações de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,36 @@
 import { app } from './app'
+import { fastifyCors } from '@fastify/cors'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { env } from './env'
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Barber API Documentation',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server is running 🚀')
 })
