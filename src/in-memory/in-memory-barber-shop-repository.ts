3ba503ce7d94d1dc8c7de1139b9139eafe4498a6
Felipe <feliperocha.dev27@gmail.com>
2025-05-28 20:26:15 -0300

    feat: implements in memory pattern

---
+++ src/in-memory/in-memory-barber-shop-repository.ts
@@ -0,0 +1,32 @@
+import type { BarberShopRepository } from '@/repositories/barber-shop-repository'
+import type { Prisma, BarberShop } from 'generated/prisma'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryBarberShopRepository implements BarberShopRepository {
+  private barberShops: BarberShop[] = []
+
+  async create(data: Prisma.BarberShopCreateInput) {
+    const barberShop = {
+      id: randomUUID(),
+      nome: data.nome,
+      email: data.email,
+      senha_hash: data.senha_hash,
+      area_atendimento: data.area_atendimento,
+      CEP: data.CEP,
+      cidade: data.cidade,
+      estado: data.estado,
+      bairro: data.bairro,
+      logradouro: data.logradouro,
+      numero: data.numero,
+      complemento: data.complemento ?? null,
+      createdAt: new Date(),
+    }
+
+    this.barberShops.push(barberShop)
+
+    return barberShop
+  }
+  findByEmail(email: string): Promise<BarberShop | null> {
+    throw new Error('Method not implemented.')
+  }
+}
