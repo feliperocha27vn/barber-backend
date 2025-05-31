ad1579c403b02ef3ccbb6748726dd88dff5c5ec4
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-05-31 11:23:03 -0300

    test: arrumando testes para trazer todas barbearias

--- src/use-cases/barber-shop/get-all-barber-shops.test.ts
+++ src/use-cases/barber-shop/get-all-barber-shops.test.ts
@@ -2,7 +2,6 @@ import { describe, it, beforeEach, expect } from 'vitest'
 import type { BarberShopRepository } from '@/repositories/barber-shop-repository'
 import { GetAllBarberShopsUseCase } from './get-all-barber-shops'
 import { InMemoryBarberShopRepository } from '@/in-memory/in-memory-barber-shop-repository'
-import { ResourceNotFoundError } from '../errors/resource-not-found-error'
 import { hash } from 'bcryptjs'
 
 let barberShopRepository: BarberShopRepository
@@ -14,7 +13,7 @@ describe('Get all barber shops', () => {
     sut = new GetAllBarberShopsUseCase(barberShopRepository)
   })
 
-  it('deve trazer todas barbearias', async () => {
+  it('deve trazer barbearias de acordo com o filtro', async () => {
     await barberShopRepository.create({
       nome: 'Barbearia do João',
       email: 'contato@barbeariadojoao.com.br',
@@ -50,5 +49,41 @@ describe('Get all barber shops', () => {
     expect(barberShops).toEqual([
       expect.objectContaining({ nome: 'Barber do Armando' }),
     ])
+  })
+
+  it('deve trazer todas barbearias', async () => {
+    await barberShopRepository.create({
+      nome: 'Barbearia do João',
+      email: 'contato@barbeariadojoao.com.br',
+      senha_hash: await hash('123', 6),
+      area_atendimento: 'Centro',
+      CEP: '01310-100',
+      estado: 'SP',
+      cidade: 'São Paulo',
+      bairro: 'Bela Vista',
+      logradouro: 'Avenida Paulista',
+      numero: '1578',
+      complemento: 'Sala 205',
+    })
+
+    await barberShopRepository.create({
+      nome: 'Barber do Armando',
+      email: 'contato@barbeariadoarmando.com.br',
+      senha_hash: await hash('123', 6),
+      area_atendimento: 'Centro',
+      CEP: '01310-100',
+      estado: 'SP',
+      cidade: 'São Paulo',
+      bairro: 'Bela Vista',
+      logradouro: 'Avenida Paulista',
+      numero: '1578',
+      complemento: 'Sala 205',
+    })
+
+    const { barberShops } = await sut.execute({
+      query: '',
+    })
+
+    expect(barberShops).toHaveLength(2)
   })
 })
